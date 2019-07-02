c9ce5c01bb04d7615f7a75acd07e99a9a82b42d0
as4691 <47302693+as4691@users.noreply.github.com>
2019-07-02 05:39:28 -0400

    Update ASH2.C

--- ASH2.C
+++ ASH2.C
@@ -1,13 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
    int s[4][2]={{24,30},{10,14},{20,35},{11,22}};
    int i,j;
    clrscr();
    for( i=0;i<=3;i++)
-    {
 	printf("\n data element %d\t%d is having %u base address",*(s[i]+1),s[i][0],s[i]);
-    }
-    getch();
+  
 }
